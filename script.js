d6bc11fdf84b733c3875df63a34bcc646553b771
Andrew Lazariev <LazarievAndrew@users.noreply.github.com>
2019-09-09 20:21:46 +0300

    ver.1.1

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 // .png,
 // .gif
 
-function getBoolean(url) {
+function isImageUrlValid(url) {
     const reg = /^https:\/\/\S+?\.(jpg|jpeg|tif|tiff|png|gif)$/;
     return reg.test(url);
 };
@@ -19,7 +19,7 @@ function getBoolean(url) {
 
 // Проверка:
 
-console.log(getBoolean('https://mirpozitiva.ru/uploads/posts/2016-08/medium/1472042492_01.jpg')); //true
-console.log(getBoolean('http://lezvia.com.ua/image/data/photo/otlichit-original-gillette-fusion-lezvia-com-ua-2.jpg')); // false //нет 's' в 'http'
-console.log(getBoolean('https://mirpozitiva.ru/uplo ads/posts/2016-08/medium/1472042492_01.jpg')); // false // есть пробел в адресе
-console.log(getBoolean('https://zabavnik.club/wp-content/uploads/2018/02/kartinki_png_1_01075113.png')); // true
+console.log(isImageUrlValid('https://mirpozitiva.ru/uploads/posts/2016-08/medium/1472042492_01.jpg')); //true
+console.log(isImageUrlValid('http://lezvia.com.ua/image/data/photo/otlichit-original-gillette-fusion-lezvia-com-ua-2.jpg')); // false //нет 's' в 'http'
+console.log(isImageUrlValid('https://mirpozitiva.ru/uplo ads/posts/2016-08/medium/1472042492_01.jpg')); // false // есть пробел в адресе
+console.log(isImageUrlValid('https://zabavnik.club/wp-content/uploads/2018/02/kartinki_png_1_01075113.png')); // true
